b0f529c8a8fc322dd704bdaf563124db7b70dfeb
yesserm <yesser.miranda0106@gmail.com>
2026-01-07 23:05:52 -0600

    Improve filtering in pdf using pdfplumber and create new pdf

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def main():
     parser.add_argument('-i', '--input', required=True, help="Path to the input PDF file")
     parser.add_argument('-o', '--output', required=False, help="Path to the output file")
     parser.add_argument('-t', '--text', required=True, help="Text for filtering pages (e.g., '3 of' or 'benefits')")
+    parser.add_argument('-r', '--reset', required=False, help="Text for resetting the filter (e.g., '1 of')")
 
     print("""
         █ Clean Sort PDF - Versión 1.0 █
@@ -18,11 +19,11 @@ def main():
     ruta_entrada = args.input
     ruta_salida = args.output if args.output else "samples\\output.pdf"
     texto_filtro = args.text if args.text else "3 of"
+    reinicio_filtro = args.reset if args.reset else "1 of"
 
     print(f"Procesando el archivo: {ruta_entrada}")
 
-    indices_paginas = utils.detectar_paginas(ruta_entrada, texto_filtro)
-
+    indices_paginas = utils.detectar_paginas_desde_filtro_reinicio(ruta_entrada, texto_filtro, reinicio_filtro)
     if indices_paginas:
         utils.crear_pdf_filtrado(ruta_entrada, indices_paginas, ruta_salida)
         print(f"PDF creado en {ruta_salida} con {len(indices_paginas)} páginas.")
--- utils.py
+++ utils.py
@@ -1,16 +1,47 @@
 import pdfplumber
 from PyPDF3 import PdfFileReader, PdfFileWriter
-
-def detectar_paginas(ruta_pdf, texto_clave): 
-    """Devuelve índices de páginas que contienen el texto clave y no están vacías.""" 
+    
+def detectar_paginas_desde_filtro(ruta_pdf, texto_clave, min_chars=100): 
+    """ Devuelve índices de páginas desde la primera que contiene el texto_clave hasta el final del documento, excluyendo las páginas con menos de min_chars caracteres. """ 
     indices = [] 
+    start_index = None 
     with pdfplumber.open(ruta_pdf) as pdf: 
         for i, pagina in enumerate(pdf.pages): 
-            contenido = pagina.extract_text()
+            contenido = pagina.extract_text() 
             if contenido and contenido.strip(): 
                 if texto_clave in contenido: 
-                    indices.append(i) 
-        return indices
+                    start_index = i 
+                    break 
+        
+        if start_index is not None: 
+            for i in range(start_index, len(pdf.pages)): 
+                contenido = pdf.pages[i].extract_text() 
+                if contenido and len(contenido.strip()) >= min_chars: 
+                    indices.append(i)
+    return indices
+
+def detectar_paginas_desde_filtro_reinicio(ruta_pdf, texto_clave = "3 of", reinicio_clave = "1 of", min_chars=100): 
+    """ Devuelve índices de páginas desde la primera que contiene el texto_clave hasta el final del documento, excluyendo las páginas con menos de min_chars caracteres. """ 
+    indices = [] 
+    start_index = None 
+    with pdfplumber.open(ruta_pdf) as pdf: 
+        for i, pagina in enumerate(pdf.pages): 
+            contenido = pagina.extract_text() 
+            if contenido and contenido.strip(): 
+                if texto_clave in contenido: 
+                    start_index = i 
+                    break 
+        
+        if start_index is not None: 
+            for i in range(start_index, len(pdf.pages)): 
+                contenido = pdf.pages[i].extract_text() 
+                if contenido and contenido.strip(): 
+                    if reinicio_clave in contenido: 
+                        stop_index = i 
+                        break 
+                    if len(contenido.strip()) >= min_chars: 
+                        indices.append(i)
+    return indices
 
 def crear_pdf_filtrado(ruta_pdf, indices, ruta_salida): 
     """Crea un nuevo PDF con las páginas seleccionadas.""" 
@@ -20,3 +51,4 @@ def crear_pdf_filtrado(ruta_pdf, indices, ruta_salida):
         writer.addPage(reader.getPage(i)) 
     with open(ruta_salida, "wb") as salida: 
         writer.write(salida)
+
